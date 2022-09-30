07ac382dde5ff8c55f1774004e5a9ac4e48d24c2
Ivan Filippov <devkit04@gmail.com>
2022-09-30 13:11:29 +0900

    Test commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def collatz(num):
+
+	res = [num]
+
+	while num > 1:
+
+		if num % 2 == 0:
+
+			num = num / 2
+
+		else:
+
+			num = 3 * num + 1
+
+		res.append(num)
+
+	return res
+
+print(collatz(5))
